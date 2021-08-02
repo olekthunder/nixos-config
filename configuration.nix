0dda28e71b5f4c2aa67fa146f4a0312151bda78e
olekthunder <zso040399@gmail.com>
2021-08-02 17:27:47 +0300

    Fix

--- configuration.nix
+++ configuration.nix
@@ -143,11 +143,11 @@ in {
       backend = "glx";
       vSync = true;
       inactiveDim = "0.2";
+      refreshRate = 0;
       extraOptions = ''
         mark-wmwin-focused = true;
         mark-ovredir-focused = true;
         detect-client-opacity = true;
-        refresh-rate = 0;
         dbe = false;
         detect-transient = true;
         detect-client-leader = true;
